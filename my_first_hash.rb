268ca6722de3547dcb9c39ba315b1b4ae304bd69
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-15 23:46:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,7 +6,7 @@ end
 
 
 def shipping_manifest
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil painting" => 3}
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
 end
 
